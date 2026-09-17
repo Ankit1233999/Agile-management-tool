50c0e3153221c110814deceffd93464c088bcb65
Ankit <sainiankit6244@gmail.com>
2026-09-17 14:15:19 +0530

    update Dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
+
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Workspace from "./pages/Workspace";
 
 function App() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-
+    <div className="app">
       <Sidebar />
-
-      <div className="flex-1">
+      <div className="main-content">
         <Navbar />
         <Dashboard />
       </div>
-
     </div>
   );
 }
